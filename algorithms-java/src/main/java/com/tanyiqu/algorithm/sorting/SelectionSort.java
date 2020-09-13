d994d7a98d061fbd1f5ad8b0c3c5e6d2684299c3
Tanyiqu <1953649096@qq.com>
2020-09-13 10:57:39 +0800

    实现选择排序

--- algorithms-java/src/main/java/com/tanyiqu/algorithm/sorting/SelectionSort.java
+++ algorithms-java/src/main/java/com/tanyiqu/algorithm/sorting/SelectionSort.java
@@ -9,7 +9,18 @@ public class SelectionSort extends AbstractSort {
 
     @Override
     void sort(Integer[] array) {
-
+        int length = array.length;
+        for (int end = length - 1; end > 0; end--) {
+            int maxIndex = 0;
+            for (int begin = 1; begin <= end; begin++) {
+                if (array[begin] > array[maxIndex]) {
+                    maxIndex = begin;
+                }
+            }
+            int temp = array[end];
+            array[end] = array[maxIndex];
+            array[maxIndex] = temp;
+        }
     }
 
 }
