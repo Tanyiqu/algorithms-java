f8395e4c731c8a84419c1a310fc37df519f9bd10
Tanyiqu <1953649096@qq.com>
2020-09-16 18:01:01 +0800

    添加测试Sort类

---
+++ algorithms-java/src/main/java/com/tanyiqu/algorithm/sorting/TestSort.java
@@ -0,0 +1,34 @@
+package com.tanyiqu.algorithm.sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tanyiqu.util.IntegerUtil;
+
+@SuppressWarnings("all")
+public class TestSort {
+
+    public static void main(String[] args) {
+        List<AbstractSort> list = new ArrayList<>();
+        list.add(new BubbleSort<>());
+        list.add(new SelectionSort<>());
+        list.add(new QuickSort<>());
+        test(list);
+    }
+
+    static void test(List<AbstractSort> list) {
+        Integer[] array = IntegerUtil.randIntegerArray(10000, 1, 10000);
+        for (AbstractSort sort : list) {
+
+            long begintime = System.currentTimeMillis();
+            sort.sort(array);
+            long endtinme = System.currentTimeMillis();
+            long costTime = (endtinme - begintime);
+
+            boolean flag = IntegerUtil.isAscend(array);
+            System.out.println("【" + sort.name() + "】" + flag + " 耗时: " + costTime + "ms");
+            System.out.println("----------------------------------");
+        }
+    }
+
+}
